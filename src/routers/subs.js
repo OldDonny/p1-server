71c2589aa3a2a4b040deefe343a7381ba9b78d48
Jord-X <jordanwhansen@gmail.com>
2017-11-17 10:57:58 -0600

    compilation of last push

--- src/routers/subs.js
+++ src/routers/subs.js
@@ -1,1 +1,10 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const subs_ctrl_1 = require("../controllers/subs.ctrl");
+const router = express_1.Router();
+router
+    .post('/', subs_ctrl_1.newFollow)
+    .delete('/:id', subs_ctrl_1.unFollow)
+    .get('/:id', subs_ctrl_1.getFollowingChirps);
+exports.default = router;
